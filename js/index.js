a5609e5c599f48b3e2477df58e670cefd0e87c55
vekee <vekee66@gmail.com>
2021-03-11 16:47:59 +0900

    モバイル端末閲覧対応

--- js/index.js
+++ js/index.js
@@ -516,7 +516,48 @@ function createPDF(downLoadFlag = true) {
 
     if (downLoadFlag) {
         if ((/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()))) {
-            alert("モバイル端末でPDFダウンロードを対応しているため、メール送信よりPDFを取得してください。")
+            alert("モバイル端末でPDF閲覧のみを対応しているため、メール送信よりPDFを取得してください。")
+            // window.open(doc.output('bloburi', { filename: timeCardPDFName }), '_blank', 'location=yes')
+            var pdfjsLib = window['pdfjs-dist/build/pdf'];
+            pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+            var loadingTask = pdfjsLib.getDocument(doc.output('bloburi', { filename: timeCardPDFName }));
+            loadingTask.promise.then(function(pdf) {
+              // you can now use *pdf* here
+            });
+
+            var loadingTask = pdfjsLib.getDocument(doc.output('bloburi', { filename: timeCardPDFName }));
+            loadingTask.promise.then(function(pdf) {
+              console.log('PDF loaded');
+              
+              // Fetch the first page
+              var pageNumber = 1;
+              pdf.getPage(pageNumber).then(function(page) {
+                console.log('Page loaded');
+                
+                var scale = 1;
+                var viewport = page.getViewport({scale: scale});
+            
+                // Prepare canvas using PDF page dimensions
+                var canvas = document.getElementById('the-canvas');
+                var context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+            
+                // Render PDF page into canvas context
+                var renderContext = {
+                  canvasContext: context,
+                  viewport: viewport
+                };
+                var renderTask = page.render(renderContext);
+                renderTask.promise.then(function () {
+                  console.log('Page rendered');
+                });
+              });
+            }, function (reason) {
+              // PDF loading error
+              console.error(reason);
+            });
+
         } else {
             doc.save(timeCardPDFName);
         }
