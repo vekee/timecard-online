05d83634ecc0cb5c628cad72f35cf48353d508d7
vekee <vekee66@gmail.com>
2021-04-17 22:20:12 +0900

    copyright add

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,6 @@
-let browser;
-var startAddress;
-var endAddress;
-//var geolocation = 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBaHq0wC0qe4x3AqnAYdQBUHCJne15ifHk';
-
 $(function() {
     $("#ssrr-footer").load("./pages/common/footer.html", function() {
         date = new Date();
-        $("#copyright").text("Copyright © " + date.getFullYear() + " 四川料理 inc. All rights Reserved.");
+        $("#copyright").text("Copyright © " + date.getFullYear() + " APASYS inc. All rights Reserved.");
     });
 });
